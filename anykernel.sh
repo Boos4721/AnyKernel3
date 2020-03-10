b15d1155d0bc5c5958177e58f47a5c7a00c73ee3
boos4721 <3.1415926535boos@gmail.com>
2020-03-10 07:09:51 +0000

    Anykernel: ?
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=HenTaiKernel by Boos4721 @ xda-developers
+kernel.string=PixelKernel by Boos4721 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus6
-device.name2=enchilada
-device.name3=OnePlus6T
-device.name4=fajita
-supported.versions=10
+device.name1=OnePlus5
+device.name2=cheeseburger
+device.name3=OnePlus5T
+device.name4=dumpling
+supported.versions=9,10
 '; } # end properties
 
 # shell variables
 block=boot;
-is_slot_device=1;
-ramdisk_compression=auto;
+is_slot_device=0;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -34,22 +33,15 @@ $bin/busybox/fstrim -v /data
 rm -rf $ramdisk/overlay.d
 
 ## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
 
 ## AnyKernel install
 dump_boot;
 
 # Add our ramdisk files if Magisk is installed
-ui_print " "; ui_print "Installing Hentai Kernel Ramdisk Files..."
-mv /tmp/anykernel/overlay.d $ramdisk/overlay.d
-cp -f /system_root/init.rc $ramdisk/overlay.d
-rm -rf /data/adb/modules/Hentai
-mkdir -p /data/adb/modules/Hentai
-cp -Rf $home/magisk/* /data/adb/modules/Hentai
-ui_print "Installing Hentai Kernel Magisk Module"
+rm -rf /data/adb/modules/Pixel
+mkdir -p /data/adb/modules/Pixel
+cp -Rf $home/magisk/* /data/adb/modules/Pixel
+ui_print "Installing Pixel Kernel Magisk Module"
 
 if [ -d $ramdisk/.backup ]; then
   patch_cmdline "skip_override" "skip_override";
@@ -60,11 +52,8 @@ fi;
 # end ramdisk changes
 
 mount -o rw,remount -t auto /system;
-chattr -R -a /system/vendor/etc/perf/;
-chattr -R -i /system/vendor/etc/perf/;
 ui_print "Installing Sqlite Files..."
 cp -rf $home/system/* /system/;
-chattr -R +i /system/vendor/etc/perf/;
 mount -o ro,remount -t auto /system;
 
 write_boot;
